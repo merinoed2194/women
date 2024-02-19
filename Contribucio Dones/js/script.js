b72a59bb0b054a138e10b766b5c7d2a3799a9902
merinoed2194 <152448429+merinoed2194@users.noreply.github.com>
2024-02-19 17:35:31 +0100

    actualizacion de modales

--- Contribucio Dones/js/script.js
+++ Contribucio Dones/js/script.js
@@ -9,17 +9,7 @@ contenedores.forEach(contenedor => {
 
 
 //SLIDER
-// document.addEventListener('DOMContentLoaded', function () {
-//     const slider = document.getElementById('slider');
-//     const images = document.querySelectorAll('#slider img');
-//     let counter = 0;
-//     function rotateSlider() {
-//         counter = (counter + 1) % images.length;
-//         const translateValue = -counter * 80;
-//         slider.style.transform = `translateX(${translateValue}%)`;
-//     }
-//     setInterval(rotateSlider, 3000); 
-// });
+
 
 document.addEventListener("DOMContentLoaded", function() {
     var slider = document.getElementById("slider");
@@ -39,35 +29,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 //Para hacer que funcionen todas las modales con el código proporcionado, puedes hacer algunos cambios en tu código JavaScript. Aquí tienes una versión ajustada que funcionará con las múltiples modales:
 
-/**MODAL V1 */
-//modal
-//Declaracion de variables
-// let modales = document.querySelectorAll('.modal');
-// let abrirBotones = document.querySelectorAll('[id^="abrir"]');
-// let cerrarBotones = document.querySelectorAll('[id^="cerrar"]');
-
-//Función abrir modal
-// abrirBotones.forEach((abrir, index) => {
-//     abrir.addEventListener('click', () => {
-//         modales[index].classList.remove('oculta-modal');
-//     });
-// });
-
-//Función cerrar modal
-// cerrarBotones.forEach((cerrar, index) => {
-//     cerrar.addEventListener('click', () => {
-//         modales[index].classList.add('oculta-modal');
-//     });
-// });
-
-// Segunda función para cerrar el modal
-// window.addEventListener('click', (e) => {
-//     modales.forEach((modal) => {
-//         if (e.target === modal) {
-//             modal.classList.add('oculta-modal');
-//         }
-//     });
-// });
 
 
 // /**MODAL V2 */
@@ -92,30 +53,6 @@ cerrarBotones.forEach((cerrar, index) => {
         modales[index].classList.add('oculta-modal');
     });
 });
-
-// //Función navegar a la modal anterior
-// anteriorBotones.forEach((anterior, index) => {
-//     anterior.addEventListener('click', () => {
-//         modales[index].classList.add('oculta-modal');
-//         if (index > 0) {
-//             modales[index - 1].classList.remove('oculta-modal');
-//         }
-//     });
-// });
-
-
-
-
-
-// // Función navegar a la modal siguiente
-// siguienteBotones.forEach((siguiente, index) => {
-//     siguiente.addEventListener('click', () => {
-//         modales[index].classList.add('oculta-modal');
-//         if (index < modales.length - 1) {
-//             modales[index + 1].classList.remove('oculta-modal');
-//         }
-//     });
-// });
 
 // Segunda función para cerrar el modal al hacer clic fuera de él
 window.addEventListener('click', (e) => {
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # women
+
 wonder
+
+
+barra division:
+
+`<a href="https://www.freepik.es/vector-gratis/diseno-fondo-tecnologia_1079348.htm#query=informatica%20png&position=0&from_view=keyword&track=ais&uuid=6ee37211-5c95-4e80-9921-9f8a24c5feb1">`Imagen de Harryarts `</a>` en Freepik
+
+
+`<a href="https://www.freepik.es/vector-gratis/modernos-elementos-tecnologicos-azules-fondo-transparente_1074435.htm#query=informatica%20png&position=9&from_view=keyword&track=ais&uuid=6ee37211-5c95-4e80-9921-9f8a24c5feb1">`Imagen de Harryarts `</a>` en Freepik
